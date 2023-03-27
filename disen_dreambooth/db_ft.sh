a6c2d39705ee4283b14fa1f811bf940b3c1dc36e
honghongzi <h-chen20@mails.tsinghua.edu.cn>
2023-03-27 09:35:16 +0000

    add disen dreambooth

---
+++ disen_dreambooth/db_ft.sh
@@ -0,0 +1,30 @@
+#https://github.com/huggingface/diffusers/tree/main/examples/dreambooth
+export MODEL_NAME="stabilityai/stable-diffusion-2-1-base"
+# export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export INSTANCE_DIR="/DATA/DATANAS1/chenhong/diffusion_research/dreambooth_data/backpack"
+export OUTPUT_DIR="./output_dreambooth"
+export CLASS_DIR="./class_data/backpack"
+
+CUDA_VISIBLE_DEVICES=1 accelerate launch dreambooth_ft.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --instance_prompt="<s1>|<s2>" \
+  --class_data_dir=$CLASS_DIR \
+  --class_prompt="backpack" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=1e-4 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=3000\
+  --save_steps=300 \
+  --lora_rank=4 \
+  --exp_name="t_dreambooth_noreg" \
+  --train_text_encoder \
+  --learning_rate_text=5e-5 \
+  # --with_prior_preservation --prior_loss_weight=0.1\
+
+ 
+
--- zero-shot-exp/z_net.py
+++ zero-shot-exp/z_net.py
@@ -36,3 +36,47 @@ class I2T_Prompt(nn.Module):
         i2t_prompt = self.direct_transfrom(img_feature)
         f_i2t_prompt = i2t_prompt.view(-1, 1, self.text_emb_size)        
         return f_i2t_prompt 
+
+class Image_adapter(nn.Module):
+    def __init__(self, emb_size=1024):
+        super().__init__()
+        ###img_latent(,4,64,64),text_latent(1, 77, 1024)
+        self.transform = nn.Sequential(
+            nn.Linear(emb_size, emb_size),
+            nn.ReLU(),
+            nn.Linear(emb_size, emb_size)
+        )
+        self.adapter_weight = 1.0
+        
+        
+        
+    def forward(self, img_feature):
+        out = img_feature + self.adapter_weight*self.transform(img_feature)
+        return out
+
+class Multimodal_adapter(nn.Module):
+    def __init__(self, emb_size=1024):
+        super().__init__()
+        self.emb_size = emb_size
+        self.K = nn.Linear(emb_size, emb_size)
+        self.Q = nn.Linear(emb_size, emb_size)
+        self.V = nn.Linear(emb_size, emb_size)
+        self.nonlinear = nn.Sequential(
+            nn.Linear(emb_size, emb_size//2),
+            nn.ReLU(),
+            nn.Linear(emb_size//2, emb_size)
+        )
+        self.img_part = Image_adapter()
+        
+        
+    def forward(self, img_feature, text_emb):
+        # img_feature: (bs, 1 ,1024), text_emb: (bs, 77, 1024)
+        img_query = self.Q( img_feature )
+        text_key = self.K( text_emb )
+        weight = (img_query*text_key).sum(2, keepdim=True)/self.emb_size**0.5 #bs,77, 1
+        img_value = self.V( img_feature ).expand(-1, text_emb.size()[1], -1) #bs, 77, 1024
+        text_img1 = text_emb + weight*img_value
+        out_feature = text_img1 + self.nonlinear(text_img1)
+        
+        out_feature = out_feature + self.img_part( img_feature ) 
+        return out_feature     
